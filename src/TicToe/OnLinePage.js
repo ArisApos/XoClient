32ee94f75699828193520e3d6e21f2ef46ebdce9
Aris <arisapostoloy@gmail.com>
2020-03-28 00:13:11 +0200

    aesthetics improvement 1

--- src/TicToe/OnLinePage.js
+++ src/TicToe/OnLinePage.js
@@ -13,15 +13,29 @@ const OnLinePage = ({ games }) => {
     <div className="onLinePage">
       <div className="controlPanel">
         <div className="charts">
-          <div className="cPlayer">Chart Player1</div>
-          <div className="cPlayer">Chart Player2</div>
-          <div className="cPlayer">Chart Player3</div>
+          <div className="cPlayer">
+            <span className="rate">1</span>
+            <span className="name">Chart Player1</span>
+          </div>
+          <div className="cPlayer">
+            <span className="rate">2</span>
+            <span className="name">Chart Player2</span>
+          </div>
+          <div className="cPlayer">
+            <span className="rate">3</span>
+            <span className="name">Chart Player3</span>
+          </div>
         </div>
         <div className="mainContent">
           <div className="status options">
             Total active players, options, others
           </div>
           <div className="timeRooms">
+            <div className="timeRoom">
+              <div className="tPlayer">Status Player1</div>
+              <div className="tPlayer">Status Player2</div>
+              <div className="tPlayer">Status Player3</div>
+            </div>
             <div className="timeRoom">
               <div className="tPlayer">Status Player1</div>
               <div className="tPlayer">Status Player2</div>
