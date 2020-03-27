10db7763b8dea00ed27a5d485ed2a21e38edc241
Aris <arisapostoloy@gmail.com>
2020-03-27 21:46:35 +0200

    create online Page

---
+++ src/TicToe/OnLinePage.js
@@ -0,0 +1,29 @@
+import React, {  } from "react";
+import { } from "./";
+import { } from "../models/tic-tac-toe/";
+import { connect } from "react-redux";
+
+//actionFunctions functions
+
+
+//Component
+const OnLinePage = ({ games }) => {
+  
+  return (
+    <>
+    <div className="onLinePage">OnLinePage</div>
+    </>
+  );
+};
+
+//ReduxState&Dispatch
+const stateOnLinePage = ({ games }) => {
+  return { };
+};
+
+const dispatchOnLinePage = dispatch => ({});
+const OnLinePageContainer = connect(
+  stateOnLinePage,
+  dispatchOnLinePage
+)(OnLinePage);
+export { OnLinePageContainer };
