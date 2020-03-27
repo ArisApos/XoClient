9f0f2b9d81ccae88b6c06c92d8218808a81c49c9
Aris <arisapostoloy@gmail.com>
2020-03-28 01:59:19 +0200

    add myStatus section and titles in each section

--- src/TicToe/OnLinePage.js
+++ src/TicToe/OnLinePage.js
@@ -13,6 +13,7 @@ const OnLinePage = ({ games }) => {
     <div className="onLinePage">
       <div className="controlPanel">
         <div className="charts">
+          <h3 className="title">Charts</h3>
           <div className="cPlayer">
             <span className="rate">1</span>
             <span className="name">Chart Player1</span>
@@ -26,19 +27,39 @@ const OnLinePage = ({ games }) => {
             <span className="name">Chart Player3</span>
           </div>
         </div>
-        <div className="mainContent">
+        <div className="myStatus">
+          <h3 className="title">My Status</h3>
+          <div className="opponents">Oponents</div>
+          <div className="points">Points</div>
+          <div className="availability">availability</div>
+          <div className="playTime">playTime</div>
+        </div>
+        <div className="liveOverview">
+          <h3 className="title">Live Overview</h3>
           <div className="status options">
             Total active players, options, others
           </div>
           <div className="timeRooms">
             <div className="timeRoom">
-              <div className="timeLimit">timeLimit</div>
+              <div className="timeLimit">timeLimit7-10</div>
               <div className="tPlayer">Status Player1</div>
               <div className="tPlayer">Status Player2</div>
               <div className="tPlayer">Status Player3</div>
             </div>
             <div className="timeRoom">
-              <div className="timeLimit">timeLimit</div>
+              <div className="timeLimit">timeLimit5-7</div>
+              <div className="tPlayer">Status Player1</div>
+              <div className="tPlayer">Status Player2</div>
+              <div className="tPlayer">Status Player3</div>
+            </div>
+            <div className="timeRoom">
+              <div className="timeLimit">timeLimit3-5</div>
+              <div className="tPlayer">Status Player1</div>
+              <div className="tPlayer">Status Player2</div>
+              <div className="tPlayer">Status Player3</div>
+            </div>
+            <div className="timeRoom">
+              <div className="timeLimit">timeLimit1-3</div>
               <div className="tPlayer">Status Player1</div>
               <div className="tPlayer">Status Player2</div>
               <div className="tPlayer">Status Player3</div>
