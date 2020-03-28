a8d0782c5703cdd2bf7bc2d331b237c77d90b292
Aris <arisapostoloy@gmail.com>
2020-03-28 17:11:41 +0200

    use static(px) values for solid alignment

--- src/TicToe/OnLinePage/ControlPanel/Ranks.js
+++ src/TicToe/OnLinePage/ControlPanel/Ranks.js
@@ -1,22 +1,25 @@
 import React, {} from 'react';
 import './Static/ranks.scss';
 
-const Ranks = ({ranks}) => {
+const mockRanks = [
+  { name: "aris", rank: "1", points: "35" },
+  { name: "ninuk", rank: "2", points: "30" },
+  { name: "makis", rank: "3", points: "25" },
+  { name: "Gogo", rank: "4", points: "20" },
+  { name: "thanos", rank: "5", points: "15" }
+];
+
+
+const Ranks = ({ranks=mockRanks}) => {
+  const rankingPlayers = ranks.map(rPlayer => (
+    <div className="rPlayer">
+      <span className="rate">{rPlayer.rank}</span>
+      <span className="name">{rPlayer.name}</span>
+    </div>
+  ));
     return (
       <section className="ranks">
-        <h3 className="title">ranks</h3>
-        <div className="rPlayer">
-          <span className="rate">1</span>
-          <span className="name">Chart Player1</span>
-        </div>
-        <div className="rPlayer">
-          <span className="rate">2</span>
-          <span className="name">Chart Player2</span>
-        </div>
-        <div className="rPlayer">
-          <span className="rate">3</span>
-          <span className="name">Chart Player3</span>
-        </div>
+       { rankingPlayers }
       </section>
     );
 }
