7e0a03c59674370a78b568b18a1098ef421f0d71
Aris <arisapostoloy@gmail.com>
2020-03-28 18:34:51 +0200

    add span error

--- src/TicToe/OnLinePage/ControlPanel/Registration.js
+++ src/TicToe/OnLinePage/ControlPanel/Registration.js
@@ -16,22 +16,31 @@ const Registration = ({ isLoggedIn=true }) => {
           placeholder="name"
           ref={register({ required: true, pattern: /^([a-zA-Z0-9_-]){3,8}$/ })}
         />
-        {errors.name &&
-          "name is required Please enter Alpanumeric Letters with length 3-8"}
+        {errors.name && (
+          <span className="error">
+            name is required Please enter Alpanumeric Letters with length 3-8
+          </span>
+        )}
         <input
           name="playersLimmit"
           placeholder="players Limmit (2-9)"
           ref={register({ required: true, pattern: /^[2-9]$/ })}
         />
-        {errors.playersLimmit &&
-          "players Limmit is required Please enter a number 2-8"}
+        {errors.playersLimmit && (
+          <span className="error">
+            players Limmit is required Please enter a number 2-8
+          </span>
+        )}
         <input
           name="timeLimmit"
           placeholder="time Limmit(2-9)'"
           ref={register({ required: true, pattern: /^[2-9]$/ })}
         />
-        {errors.timeLimmit &&
-          "time Limmit is required Please enter a number 2-10"}
+        {errors.timeLimmit && (
+          <span className="error">
+            time Limmit is required Please enter a number 2-9
+          </span>
+        )}
         <input className="submit" type="submit" />
       </form>
     </section>
