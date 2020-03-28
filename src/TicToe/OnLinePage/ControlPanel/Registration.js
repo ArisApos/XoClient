a9cba9e55e7a4ce946c0d0de23068c4fb933b431
Aris <arisapostoloy@gmail.com>
2020-03-28 18:14:28 +0200

    upgrade the Registration by using form

--- src/TicToe/OnLinePage/ControlPanel/Registration.js
+++ src/TicToe/OnLinePage/ControlPanel/Registration.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import "./Static/registration.scss";
 
-const Registration = ({ isLoggedIn }) => {
+const Registration = ({ isLoggedIn=true }) => {
+      const { register, handleSubmit, errors } = useForm(); // initialise the hook
+      const onSubmit = data => {console.log(data);};
   return (
-    <section className="registration">
+    <section
+      className={isLoggedIn ? "registration isLoggedIn" : "registration"}
+    >
       <h3 className="title">Register</h3>
-      <div className="statusItem opponents">Oponents</div>
-      <div className="statusItem points">Points</div>
-      <div className="statusItem availability">availability</div>
-      <div className="statusItem playTime">playTime</div>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="name"
+          placeholder="name"
+          ref={register({ required: true, pattern: /^([a-zA-Z0-9_-]){3,8}$/ })}
+        />
+        {errors.name &&
+          "name is required Please enter Alpanumeric Letters with length 3-8"}
+        <input
+          name="playersLimmit"
+          placeholder="players Limmit (2-9)"
+          ref={register({ required: true, pattern: /^[2-9]$/ })}
+        />
+        {errors.playersLimmit &&
+          "players Limmit is required Please enter a number 2-8"}
+        <input
+          name="timeLimmit"
+          placeholder="time Limmit(2-9)'"
+          ref={register({ required: true, pattern: /^[2-9]$/ })}
+        />
+        {errors.timeLimmit &&
+          "time Limmit is required Please enter a number 2-10"}
+        <input className="submit" type="submit" />
+      </form>
     </section>
   );
 };
