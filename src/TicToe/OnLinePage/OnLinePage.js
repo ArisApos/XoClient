e5f7d8684cf212ad5da3b0b3a6419c73c176d3e1
Aris <arisapostoloy@gmail.com>
2020-03-29 18:21:55 +0300

    onLine page gets the full socket events API from server and pass it to the children

--- src/TicToe/OnLinePage/OnLinePage.js
+++ src/TicToe/OnLinePage/OnLinePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect  } from "react";
 import { ControlPanelContainer } from "./ControlPanel";
 import { } from "../../models/tic-tac-toe";
+import { ss, cs, ENTRY_POINT } from "../../models/onLine";
 import { connect } from "react-redux";
 import socketIOClient from "socket.io-client";
 import './Static/onlinePage.scss';
@@ -10,31 +11,27 @@ import './Static/onlinePage.scss';
 
 //Component
 const OnLinePage = ({ games }) => {
-  const [socketData, setSocketData] = useState({
-    endpoint: "http://127.0.0.1:5000",
-    socket: null,
-    connected: false,
-    id: null
-  });
+  const [socketData, setSocketData] = useState({ ss, cs, socket:null, connected: false, id: null });
   const [onlinePlayers, setOnlinePlayers] = useState(null);
   useEffect(() => {
-    const socket = socketIOClient(socketData.endpoint);
-    socket.on("sUpdatePlayers",({players})=>{
+    const socket = socketIOClient(ENTRY_POINT);
+    socket.on(ss.root.CONNECTION_REPLY, ({ ss, cs, id, connected, players }) => {
+      console.log({ss, cs, id, connected, players });
+      setSocketData({ ...socketData, ss, cs, socket, id, connected });
+      socket.emit(cs.root.CONNECTION_REPLY, {
+        data: "Dude!!! React Client"
+      });
+    });
+    socket.on(ss.root.UPDATE_PLAYERS,({players})=>{
       console.log('Broaaaadcasstttted', players)
       setOnlinePlayers(players);
     });
-    socket.on("sConnectionReply", ({id, connected, players}) => {
-      console.log({id, connected, players });
-      setSocketData({ ...socketData, socket, id, connected });
-      socket.emit("cConnectionReply", {
-        data: "Dude!!! This is awesome, from React Client"
-      });
-    });
+
   }, []);
 
   return (
     <section className="onLinePage">
-      <ControlPanelContainer socket={socketData.socket} />
+      <ControlPanelContainer socketData={socketData} />
       {socketData.connected && (
         <div className="connectionIndecation">{socketData.id}</div>
       )}
