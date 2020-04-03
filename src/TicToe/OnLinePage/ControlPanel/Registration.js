4ff39c3ec2d5c38cc7d20ff449b21080878fe892
Aris <arisapostoloy@gmail.com>
2020-04-03 16:42:24 +0300

    create wrappers

--- src/TicToe/OnLinePage/ControlPanel/Registration.js
+++ src/TicToe/OnLinePage/ControlPanel/Registration.js
@@ -26,61 +26,72 @@ const Registration = ({socketData:{socket, ss, cs}, on=true }) => {
     <section className={on ? "registration on" : "registration"}>
       <h3 className="title">Register</h3>
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className={errors.name ? "errorInput" : null}
-          name="name"
-          placeholder="name"
-          ref={register({ required: true, pattern: /^([a-zA-Z0-9_-]){3,8}$/ })}
-        />
-        {errors.name && (
-          <span className="error">
-            name is required Please enter Alpanumeric Letters with length 3-8
-          </span>
-        )}
-        <input
-          className={errors.password ? "errorInput" : null}
-          name="password"
-          placeholder="password"
-          ref={register({
-            required: true,
-            pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
-          })}
-        />
-        {errors.password && (
-          <span className="error">
-            Password must contain
-            <br />
-            1)At least 1 lowercase alphabetical character
-            <br />
-            2)at least 1 uppercase alphabetical character
-            <br />
-            3)at least 1 numeric character
-            <br />
-            4)at least one special character(!@#$%^&*)
-          </span>
-        )}
-        <input
-          className={errors.playersLimmit ? "errorInput" : null}
-          name="playersLimmit"
-          placeholder="players Limmit (2-9)"
-          ref={register({ required: true, pattern: /^[2-9]$/ })}
-        />
-        {errors.playersLimmit && (
-          <span className="error">
-            players Limmit is required Please enter a number 2-8
-          </span>
-        )}
-        <input
-          className={errors.timeLimmit ? "errorInput" : null}
-          name="timeLimmit"
-          placeholder="time Limmit(2-9)'"
-          ref={register({ required: true, pattern: /^[2-9]$/ })}
-        />
-        {errors.timeLimmit && (
-          <span className="error">
-            time Limmit is required Please enter a number 2-9
-          </span>
-        )}
+        <div className="inputField">
+          <input
+            className={errors.name ? "errorInput" : null}
+            name="name"
+            placeholder="name"
+            ref={register({
+              required: true,
+              pattern: /^([a-zA-Z0-9_-]){3,8}$/
+            })}
+          />
+          {errors.name && (
+            <span className="error">
+              name is required Please enter Alpanumeric Letters with length 3-8
+            </span>
+          )}
+        </div>
+        <div className="inputField">
+          <input
+            className={errors.password ? "errorInput" : null}
+            name="password"
+            placeholder="password"
+            ref={register({
+              required: true,
+              pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+            })}
+          />
+          {errors.password && (
+            <span className="error">
+              Password must contain
+              <br />
+              1)At least 1 lowercase alphabetical character
+              <br />
+              2)at least 1 uppercase alphabetical character
+              <br />
+              3)at least 1 numeric character
+              <br />
+              4)at least one special character(!@#$%^&*)
+            </span>
+          )}
+        </div>
+        <div className="inputField">
+          <input
+            className={errors.playersLimmit ? "errorInput" : null}
+            name="playersLimmit"
+            placeholder="players Limmit (2-9)"
+            ref={register({ required: true, pattern: /^[2-9]$/ })}
+          />
+          {errors.playersLimmit && (
+            <span className="error">
+              players Limmit is required Please enter a number 2-8
+            </span>
+          )}
+        </div>
+        <div className="inputField">
+          <input
+            className={errors.timeLimmit ? "errorInput" : null}
+            name="timeLimmit"
+            placeholder="time Limmit(2-9)'"
+            ref={register({ required: true, pattern: /^[2-9]$/ })}
+          />
+          {errors.timeLimmit && (
+            <span className="error">
+              time Limmit is required Please enter a number 2-9
+            </span>
+          )}
+        </div>
         <input className="submit" type="submit" />
         {responseData.response && <div>{responseData.response}</div>}
       </form>
