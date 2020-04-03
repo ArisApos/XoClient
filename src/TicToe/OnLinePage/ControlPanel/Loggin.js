38c12acbfad9caf6c0061bcaf1b60d020899ad81
Aris <arisapostoloy@gmail.com>
2020-04-03 16:41:47 +0300

    build the loggin1

--- src/TicToe/OnLinePage/ControlPanel/Loggin.js
+++ src/TicToe/OnLinePage/ControlPanel/Loggin.js
@@ -25,42 +25,22 @@ const Loggin = ({
   };
 
   return (
-    <section
-      className={on ? "login on" : "login"}
-    >
+    <section className={on ? "login on" : "login"}>
       <h3 className="title">Loggin</h3>
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <input
-          name="name"
-          placeholder="name"
-          ref={register({ required: true, pattern: /^([a-zA-Z0-9_-]){3,8}$/ })}
-        />
-        {errors.name && (
-          <span className="error">
-            name is required Please enter Alpanumeric Letters with length 3-8
-          </span>
-        )}
-        <input
-          name="password"
-          placeholder="password"
-          ref={register({
-            required: true,
-            pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
-          })}
-        />
-        {errors.password && (
-          <span className="error">
-            Password must contain
-            <br />
-            1)At least 1 lowercase alphabetical character
-            <br />
-            2)at least 1 uppercase alphabetical character
-            <br />
-            3)at least 1 numeric character
-            <br />
-            4)at least one special character(!@#$%^&*)
-          </span>
-        )}
+        <div className="inputField">
+          <input
+            name="name"
+            placeholder="name"
+          />
+        </div>
+        <div className="inputField">
+          <input
+            name="password"
+            placeholder="password"
+          />
+        </div>
+        {responseData.response && <div>wrong username or password</div>}
         <input className="submit" type="submit" />
         {responseData.response && <div>{responseData.response}</div>}
       </form>
