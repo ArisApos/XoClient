c75579dd6df0ef70aa8e5f04bd665c3aaa93eeba
Aris <arisapostoloy@gmail.com>
2020-04-03 13:46:41 +0300

    add loggin

--- src/TicToe/OnLinePage/ControlPanel/index.js
+++ src/TicToe/OnLinePage/ControlPanel/index.js
@@ -1,5 +1,6 @@
 export { Ranks } from './Ranks';
 export { MyStatus } from './MyStatus';
 export { Registration } from './Registration';
+export { Loggin } from './Loggin';
 export { LiveOverview } from './LiveOverview';
 export { ControlPanelContainer } from './ControlPanel'
