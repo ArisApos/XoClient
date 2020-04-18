d8920eeabcf0df702ec54a4255b0872ef9ec0bc8
Aris <arisapostoloy@gmail.com>
2020-04-19 01:31:44 +0300

    use right structure

--- src/TicToe/OnLinePage/Common/ServerNotification.js
+++ src/TicToe/OnLinePage/Common/ServerNotification.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { connect } from "react-redux";
 
-let ServerNotification = ({ serverNotification:{ requesting, message, success } }) => {
+let ServerNotification = ({ requesting, message, success }) => {
     const loaderContent = requesting ? <div className='loaderContainer'><div className='loader'></div></div> : null;
     const messageContent = message && !requesting ? <div className={ success? "response" : "response fail" }>{ message }</div> : null;   
     return { loaderContent, messageContent, success };
 };
 
 // ReduxState&Dispatch
-const stateServerNotification = ({ serverNotification }) => {
-  return { serverNotification };
+const stateServerNotification = ({ online: {serverNotification} }) => {
+  return { ...serverNotification };
 };
 
 ServerNotification = connect(stateServerNotification)(ServerNotification);
