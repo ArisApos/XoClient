14f5b34a396e14dcca0a7b80b2155d32e614b7bd
Aris <arisapostoloy@gmail.com>
2020-04-18 23:34:47 +0300

    saga for loggin

--- src/models/onLine/index.js
+++ src/models/onLine/index.js
@@ -5,3 +5,5 @@ export { SET_LOGGED_STATUS, SET_PLAYER_STATUS, SET_PLAYERS } from "./actionTypes
 export { setPlayerLoggedStatus, setPlayerStatus, setPlayers } from "./actions";
 
 export { online } from './reducers';
+
+export { rootSaga } from './sagas';
--- src/models/onLine/sagas.js
+++ src/models/onLine/sagas.js
@@ -1,13 +1,29 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
-import { ENTRY_POINT } from "./";
-import { axios } from 'axios';
+import { GET_PLAYER_REQUESTED } from './actionTypes';
+import { call, put, takeLatest, all } from 'redux-saga/effects';
+import { axiosApi } from './axiosApi';
 
-// Worker saga
-function* getPlayer(action){
-    const { name, password } = action.payload;
+// Worker, Wathcer getPlayer saga
+function* getPlayer(action) {
+    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@getPlayer', action);
+    const { name, password, endpoint, method } = action;
+    const data = { name, password };
+    console.log('action llllllllllllllllll',data, endpoint, method)
     try {
-      const player = yield call(axios.get)    
-    } catch(err) {
-        
+      yield put({type:'SUCCESSSSSSSS', data, name, password, endpoint, method});    
+    //   const player = yield call(axiosApi({ data, method, endpoint }));
+    }catch(err) {
+      yield put({type:'FAILLLLLLL'});
     }
 }
+function* watchGetPlayer(action) {
+    console.log('watchGetPlayer', action);
+    yield takeLatest( GET_PLAYER_REQUESTED, getPlayer);
+}
+
+
+// RootSaga
+function* rootSaga() {
+  yield all([ watchGetPlayer()])
+}
+
+export { rootSaga };
