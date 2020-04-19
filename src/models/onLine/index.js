e4a84962635b0ec9629c0b5778dd17b655295447
Aris <arisapostoloy@gmail.com>
2020-04-20 00:23:22 +0300

    add actions

--- src/models/onLine/index.js
+++ src/models/onLine/index.js
@@ -2,7 +2,7 @@ export { ss, cs, ENTRY_POINT } from './sockets';
 
 export { SET_LOGGED_STATUS, SET_PLAYER_STATUS, SET_PLAYERS } from "./actionTypes";
 
-export { setPlayerLoggedStatus, setPlayerStatus, setPlayers } from "./actions";
+export { setPlayerLoggedStatus, setPlayerStatus, setPlayers, getPlayerRequested, postPlayerRequested } from "./actions";
 
 export { online } from './reducers';
 
