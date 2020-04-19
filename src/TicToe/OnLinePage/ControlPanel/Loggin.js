042a5ec3b272fe7b187254fbd0b58b2fd8a12577
Aris <arisapostoloy@gmail.com>
2020-04-20 00:22:15 +0300

    Loggin refactor, sagas,use serverNotification

--- src/TicToe/OnLinePage/ControlPanel/Loggin.js
+++ src/TicToe/OnLinePage/ControlPanel/Loggin.js
@@ -1,60 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { batch } from 'react-redux';
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import axios from "axios";
-import { ENTRY_POINT } from "../../../models/onLine";
 import { ServerNotification } from "../Common";
 import "./Static/loggin.scss";
 
 
-const Loggin = ({ on, setActiveWindows, setPlayerLoggedStatus, setPlayerStatus }) => {
+const Loggin = ({ on, setActiveWindows, getPlayerRequested }) => {
+  const [userLocationHere, setUserLocationHere] = useState(true);
   const { Loader, Message, success } = ServerNotification();
-
-  const [responseData, setResponseData] = useState({ response: false });
-  const [loader, setLoader] = useState(false);
+  const { register, handleSubmit, reset } = useForm();
   const goRegistration = ()=>setActiveWindows({registration:true, loggin: false});
-  useEffect(() => {}, []);
-  const { register, handleSubmit, reset } = useForm(); // initialise the hook
+  // Go player status
   const deactivateBoth = ()=>setActiveWindows({ registration: false, loggin: false });
-  const getAllPlayers = (tokken) =>
-    axios.get(ENTRY_POINT+'/players' , { headers: { Authorization:`Bearer ${tokken}`} })
-    .then(res=> {
-      console.log('aaaallllll',res.data)
-    })
-    .catch(err=>{
-      console.log('errrrr',err);
-    })
+  // Go to player status and reset form
+  if(success && userLocationHere){
+    setUserLocationHere(false);
+    setTimeout(()=> { reset(); deactivateBoth();},1000);
+  } 
+
+
+  // const getAllPlayers = (tokken) =>()
+    // axios.get(ENTRY_POINT+'/players' , { headers: { Authorization:`Bearer ${tokken}`} })
+    // .then(res=> {
+    //   console.log('aaaallllll',res.data)
+    // })
+    // .catch(err=>{
+    //   console.log('errrrr',err);
+    // })
   const onSubmit = data => {
-    console.log(data);
-    setLoader(true);
     const { name, password } = data;
-    const encodedPassword = encodeURIComponent(password);
-    const encodedUri = `${ENTRY_POINT}/players/${name}/${encodedPassword}`;
-    axios.get(encodedUri)
-      .then(res=> {
-        if (res.data.authSuccess) {
-          setResponseData({ response: true, data: {authSuccess:res.data.authSuccess, message: res.data.message} });
-          console.log("GetPlayer-----success-", res.data);
-          console.log("aaaallll players", getAllPlayers(res.data.token));
-          deactivateBoth();
-          batch(()=>{
-            setPlayerStatus(res.data.status);
-            setPlayerLoggedStatus(true, res.data.token);
-          });
-          reset();
-          setResponseData({ response: false });
-        } else {
-             console.log('problem');
-             setResponseData({ response: true, data: {authSuccess:res.data.authSuccess, message:res.data.message} });
-           }
-           setLoader(false);
-         })
-         .catch(err=>{
-           console.log('ffffffffffffffff',err.message, err.body, err.data, err);
-           setResponseData({ response: true, data: {authSuccess: false, message:'Internal error server'} });
-           setLoader(false);
-         });
-    
+    const endpoint = 'players';
+    const method = 'get'
+    const userLocation = 'loggin';
+    getPlayerRequested(name,password,endpoint,method,userLocation);
   };
   return (
     <section className={on ? "login on" : "login"}>
