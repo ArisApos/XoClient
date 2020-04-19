19413ee25e3c1adbde4c34f7e662f5bf95dadf55
Aris <arisapostoloy@gmail.com>
2020-04-20 00:22:31 +0300

    getm post player actions

--- src/models/onLine/actions.js
+++ src/models/onLine/actions.js
@@ -4,6 +4,8 @@ import {
   SET_PLAYER_STATUS,
   SET_PLAYERS,
   SET_SERVER_NOTIFICATION,
+  GET_PLAYER_REQUESTED,
+  POST_PLAYER_REQUESTED,
 } from "./actionTypes";
 
 const setPlayerLoggedStatus = actionCreator(SET_LOGGED_STATUS, 'loggedIn', 'token');
@@ -12,11 +14,17 @@ const setPlayerStatus = actionCreator(SET_PLAYER_STATUS, 'payload');
 
 const setPlayers = actionCreator(SET_PLAYERS, 'players');
 
-const setServerNotificatons = actionCreator(SET_SERVER_NOTIFICATION, 'requesting', 'message', 'success');
+const setServerNotificatons = actionCreator(SET_SERVER_NOTIFICATION, 'requesting', 'message', 'success', 'userLocation');
+
+const getPlayerRequested = actionCreator(GET_PLAYER_REQUESTED, 'name', 'password', 'endpoint', 'method', 'userLocation');
+
+const postPlayerRequested = actionCreator(POST_PLAYER_REQUESTED, 'formData', 'endpoint', 'method', 'headers', 'userLocation' );
 
 export {
   setPlayerLoggedStatus,
   setPlayerStatus,
   setPlayers,
   setServerNotificatons,
+  getPlayerRequested,
+  postPlayerRequested,
 };
