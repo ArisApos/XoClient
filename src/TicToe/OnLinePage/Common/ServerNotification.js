22c84e8166d5565592bb8d85c8c9de975b8ac0b7
Aris <arisapostoloy@gmail.com>
2020-04-20 18:38:23 +0300

    build with identifier

--- src/TicToe/OnLinePage/Common/ServerNotification.js
+++ src/TicToe/OnLinePage/Common/ServerNotification.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { useSelector, shallowEqual } from "react-redux";
 
 const ServerNotification = () => {
-  const { userLocation, requesting, message, success } = useSelector(state=>state.online.serverNotification, shallowEqual);
+  const { identifier, requesting, message, success } = useSelector(state=>state.online.serverNotification, shallowEqual);
   console.log('////////////=>ServerNotification', requesting, message, success);
     const Loader = requesting ? <div key='loader' className='loaderContainer'><div className='loader'></div></div> : null;
     const Message = message && !requesting ? <div key='message' className={ success? "response" : "response fail" }>{ message }</div> : null;   
-    return { userLocation, Loader, Message, success };
+    return { identifier, Loader, Message, success };
 };
 
 export { ServerNotification };
