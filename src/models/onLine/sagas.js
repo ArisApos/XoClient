9b7afc6646126088193c9c331c02a9f59017d83b
Aris <arisapostoloy@gmail.com>
2020-04-20 18:37:46 +0300

    postPlayer saga

--- src/models/onLine/sagas.js
+++ src/models/onLine/sagas.js
@@ -1,34 +1,54 @@
-import { GET_PLAYER_REQUESTED } from './actionTypes';
+import { GET_PLAYER_REQUESTED, POST_PLAYER_REQUESTED } from './actionTypes';
 import { setServerNotificatons,setPlayerStatus, setPlayerLoggedStatus, setPlayers } from './actions';
 import { call, put, takeLatest, all } from 'redux-saga/effects';
 import { axiosApi } from './axiosApi';
 
 // Worker, Wathcer getPlayer saga
 function* getPlayer(action) {
-    const { name, password, endpoint, method, userLocation } = action;
+    const { name, password, endpoint, method, identifier } = action;
     const data = { name, password };
-    console.log(action)
-    yield put(setServerNotificatons(true))
+    yield put(setServerNotificatons(true, undefined, undefined, identifier))
     try { 
-      const  {message, authSuccess, token, status} = yield call(axiosApi, {data, method, endpoint });
+      const  {message, token, status} = yield call(axiosApi, { method, endpoint, data });
+      identifier.loggedIn = true;
       yield all([
-          put(setServerNotificatons(false, message, authSuccess, userLocation )),
+          put(setServerNotificatons(false, message, true, identifier )),
           put(setPlayerLoggedStatus(true, token)),
           put(setPlayerStatus(status))
       ]);
     }catch(err) {
     const { message } = err;
-    yield put(setServerNotificatons(false, message, false ));
+    yield put(setServerNotificatons(false, message, false, identifier ));
     }
 }
+
 function* watchGetPlayer() {
     yield takeLatest( GET_PLAYER_REQUESTED, getPlayer);
 }
 
+// Worker, Wathcer postPlayer saga
+function* postPlayer(action) {
+    const { endpoint, data, method, identifier, headers } = action;
+    console.log('****************PostPlayerWorker Action',action);
+    yield put(setServerNotificatons(true, null, null, identifier))
+    try {
+        const { message } = yield call(axiosApi, {method, endpoint, data, headers});
+       yield put(setServerNotificatons(false, message, true, identifier));
+    }catch(err) {
+        const { message } = err;
+        yield put(setServerNotificatons(false, message, false, identifier ));
+    }
+}
+
+function* watchPostPlayer() {
+    yield takeLatest( POST_PLAYER_REQUESTED, postPlayer);
+}
+
+
 
 // RootSaga
 function* rootSaga() {
-  yield all([ watchGetPlayer()])
+  yield all([ watchGetPlayer(), watchPostPlayer()])
 }
 
 export { rootSaga };
