70a486b512a105f9d827cfbd7784508dc6bdfa09
Aris <arisapostoloy@gmail.com>
2020-04-24 20:05:12 +0300

    socket Related

--- src/models/onLine/libs/sockets.js
+++ src/models/onLine/libs/sockets.js
@@ -1,3 +1,5 @@
+import socketIOClient from "socket.io-client";
+
 const ENTRY_POINT = "http://127.0.0.1:5000";
 const ss = {
   root: {
@@ -13,5 +15,21 @@ const cs = {
     CONNECTION_REPLY: "CONNECTION_REPLY",
   }
 };
+const socketConnection = async (name,password,token)=>{
+  const socket = socketIOClient(ENTRY_POINT);
+  // const socket = io(ENTRY_POINT);
+  return await new Promise((resolve)=>{
+      socket.on(ss.root.CONNECTION_REPLY, (socketData) => {
+        socketData.socket = socket;
+        console.log(">>>>>>>SOCKET CONNECTION,socketData", socketData);
+        socket.emit(cs.root.CONNECTION_REPLY, {
+          message:
+            "Dude!!! I am a React Client and i am connected to you!!!!Take my name,my password and my token",
+          data: { name, password, token },
+        });
+        resolve(socketData)
+      });
+  });
+}
 
-export { ss, cs, ENTRY_POINT };
+export { ss, cs, ENTRY_POINT, socketConnection };
