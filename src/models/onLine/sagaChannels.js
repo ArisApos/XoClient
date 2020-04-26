a91a1b9c250cea0b9f494641aa17b24e6f892f43
Aris <arisapostoloy@gmail.com>
2020-04-26 22:46:56 +0300

    prepair sagaChannel

--- src/models/onLine/sagaChannels.js
+++ src/models/onLine/sagaChannels.js
@@ -1,46 +1,34 @@
-import { take, put, call, apply, delay } from 'redux-saga/effects'
-import { eventChannel } from 'redux-saga'
-import { createSocketConnection } from './libs';
+import { take, put, call, apply, delay, fork } from 'redux-saga/effects';
+import { eventChannel } from 'redux-saga';
+import { createSocketConnection } from './libs/sockets'
 
-function createSocketChannel(socket) {
+function createSocketChannel({socket, ss}) {
 
   return eventChannel(emit => {
 
-    const pingHandler = (event) => {
-      emit(event.payload)
+    const updatePlayers = ({players}) => {
+      emit({ type: ss.root.UPDATE_PLAYERS, payload: players });
     }
-
-    const errorHandler = (errorEvent) => {
-      emit(new Error(errorEvent.reason))
-    }
-
-    socket.on('ping', pingHandler)
-    socket.on('error', errorHandler)
-
+    socket.on(ss.root.UPDATE_PLAYERS, updatePlayers);
     const unsubscribe = () => {
-      socket.off('ping', pingHandler)
     }
-
-    return unsubscribe
+    return unsubscribe;
   })
 }
 
-// reply with a `pong` message by invoking `socket.emit('pong')`
-function* pong(socket) {
-  yield delay(5000)
-  yield apply(socket, socket.emit, ['pong']) // call `emit` as a method with `socket` as context
-}
+// function* pong(socket) {
+//   yield apply(socket, socket.emit, ['pong']) // call `emit` as a method with `socket` as context
+// }
 
-export function* watchOnPings() {
-  const socket = yield call(createWebSocketConnection)
-  const socketChannel = yield call(createSocketChannel, socket)
+function* watchSocketServer(socketData) {
+
+  const socketChannel = yield call(createSocketChannel, socketData);
 
   while (true) {
     try {
-      // An error from socketChannel will cause the saga jump to the catch block
-      const payload = yield take(socketChannel)
-      yield put({ type: INCOMING_PONG_PAYLOAD, payload })
-      yield fork(pong, socket)
+      const {type, payload} = yield take(socketChannel)
+      yield put({ type, payload })
+    //   yield fork(pong, socket)
     } catch(err) {
       console.error('socket error:', err)
       // socketChannel is still open in catch block
@@ -49,3 +37,5 @@ export function* watchOnPings() {
     }
   }
 }
+
+export { watchSocketServer };
