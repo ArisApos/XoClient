7feb6df9451d51bc03b75bc6ffa6c3c8c21a1bd1
Aris <arisapostoloy@gmail.com>
2020-04-26 23:30:26 +0300

    remove unnecessary

--- src/models/onLine/libs/index.js
+++ src/models/onLine/libs/index.js
@@ -1,3 +1,3 @@
 export { indexing } from './indexing';
 export { axiosApi } from './axiosApi';
-export { ss, cs, ENTRY_POINT, createSocketConnection } from "./sockets";
+export { ENTRY_POINT, createSocketConnection } from "./sockets";
