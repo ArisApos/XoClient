bfeedb2eaf4492a2ec2ddd0595125266a721285b
Aris <arisapostoloy@gmail.com>
2020-04-26 23:30:44 +0300

    remove unnecessary

--- src/models/onLine/libs/sockets.js
+++ src/models/onLine/libs/sockets.js
@@ -1,35 +1,17 @@
 import socketIOClient from "socket.io-client";
 
 const ENTRY_POINT = "http://127.0.0.1:5000";
-const ss = {
-  root: {
-    NAME: "/",
-    UPDATE_PLAYERS: "UPDATE_PLAYERS",
-    CONNECTION_REPLY: "CONNECTION_REPLY",
-  }
-};
-// Client Name Spaces with Events
-const cs = {
-  root: {
-    NAME: "/",
-    CONNECTION_REPLY: "CONNECTION_REPLY",
-  }
-};
-const createSocketConnection = async (name,password,token)=>{
+
+const createSocketConnection = async ()=>{
   // const socket = socketIOClient(ENTRY_POINT);
   const socket = socketIOClient(ENTRY_POINT);
   return await new Promise((resolve)=>{
       socket.on("connection", (socketData) => {
         socketData.socket = socket;
-        console.log(">>>>>>>SOCKET CONNECTION,socketData", socketData);
-        socket.emit("passIdentity", {
-          message:
-            "Dude!!! I am a React Client and i am connected to you!!!!Take my name,my password and my token",
-          data: { name, password, token },
-        });
+        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SOCKET CONNECTION,socketData", socketData);
         resolve(socketData)
       });
   });
 }
 
-export { ss, cs, ENTRY_POINT, createSocketConnection };
+export { ENTRY_POINT, createSocketConnection };
