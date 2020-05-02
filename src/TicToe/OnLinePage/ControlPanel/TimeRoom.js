9a12ecc7545a94158079f9306b826fab276a1782
Aris <arisapostoloy@gmail.com>
2020-05-02 15:54:37 +0300

    rPlayer Chalenge a player Triggering

--- src/TicToe/OnLinePage/ControlPanel/TimeRoom.js
+++ src/TicToe/OnLinePage/ControlPanel/TimeRoom.js
@@ -1,6 +1,6 @@
 import React from 'react';
-const TimeRoom = (roomPlayers) => {
-    const tPlayers = roomPlayers.map(({name})=><div key={name} className="tPlayer">{name}</div>)
+const TimeRoom = (roomPlayers, onClickPlayer) => {
+    const tPlayers = roomPlayers.map(({name})=><div key={name} data-key={name} onClick={onClickPlayer} className="tPlayer">{name}</div>)
  return <div key={roomPlayers[0].maxTime} className="timeRoom">
     <div className="timeLimit">{roomPlayers[0].maxTime}</div>
     { tPlayers }
