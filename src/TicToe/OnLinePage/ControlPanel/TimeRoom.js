03e70345f4a8594251b2c997d278e5829e620332
Aris <arisapostoloy@gmail.com>
2020-05-04 22:15:40 +0300

    enemy theme indication

--- src/TicToe/OnLinePage/ControlPanel/TimeRoom.js
+++ src/TicToe/OnLinePage/ControlPanel/TimeRoom.js
@@ -1,6 +1,11 @@
 import React from 'react';
-const TimeRoom = (roomPlayers, onClickPlayer) => {
-    const tPlayers = roomPlayers.map(({name})=><div key={name} data-key={name} onClick={onClickPlayer} className="tPlayer">{name}</div>)
+const TimeRoom = (roomPlayers, onClickPlayer, enemies) => {
+    const tPlayers = roomPlayers.map(
+      ({name})=><div key={name}
+                     data-key={name}
+                     onClick={onClickPlayer}
+                     className={enemies.find(enemy=>enemy===name) ? "tPlayer enemy" : "tPlayer"}>{name}</div>
+      );
  return <div key={roomPlayers[0].maxTime} className="timeRoom">
     <div className="timeLimit">{roomPlayers[0].maxTime}</div>
     { tPlayers }
