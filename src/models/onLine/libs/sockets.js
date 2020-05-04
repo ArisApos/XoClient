e5eb59ef782af86bfc2c30e98cdd0dfb64ff7101
Aris <arisapostoloy@gmail.com>
2020-05-04 09:26:24 +0300

    refactor for create socket in dynamic nameSpaces

--- src/models/onLine/libs/sockets.js
+++ src/models/onLine/libs/sockets.js
@@ -2,13 +2,14 @@ import socketIOClient from "socket.io-client";
 
 const ENTRY_POINT = "http://127.0.0.1:5000";
 
-const createSocketConnection = async ()=>{
-  // const socket = socketIOClient(ENTRY_POINT);
-  const socket = socketIOClient(ENTRY_POINT);
+const createSocketConnection = async ({nameSpace})=>{
+  const url = nameSpace ? ENTRY_POINT+nameSpace : ENTRY_POINT;
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>createSocketConnection  url',url)
+  const socket = socketIOClient(url);
   return await new Promise((resolve)=>{
       socket.on("connection", (socketData) => {
         socketData.socket = socket;
-        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SOCKET CONNECTION,socketData", socketData);
+        console.log(">>>>>>>>>>>>>>>>>>>>>>>>SERVER!!!!! SOCKET CONNECTION to url,NameSsocketData",url, socketData);
         resolve(socketData)
       });
   });
