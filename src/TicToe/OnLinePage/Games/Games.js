bfd7c521b47c7018d6eb6f1c04e97fa278377fd8
Aris <arisapostoloy@gmail.com>
2020-05-08 00:44:16 +0300

    do not use root socket

--- src/TicToe/OnLinePage/Games/Games.js
+++ src/TicToe/OnLinePage/Games/Games.js
@@ -4,8 +4,8 @@ import { Game } from './Game';
 import './static/games.scss'
 
 const Games = () => {
-    const { games, socketData } = useSelector(state=>({games:Object.values(state.online.games),socketData:state.online.socketData}));
-    const gamesEls = games.map(game=><Game key={game.name} game={{...game}} socketData={{...socketData}}/>);
+    const { games } = useSelector(state=>({games:Object.values(state.online.games)}));
+    const gamesEls = games.map(game=><Game key={game.name} {...game}/>);
     return (
       <div className="gamesContainer">
           {gamesEls}
