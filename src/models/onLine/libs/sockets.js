54bb9ecd0e08ded0a4bb4a1f93962c2384eea974
Aris <arisapostoloy@gmail.com>
2020-08-02 13:00:01 +0300

    changed serving preferences

--- src/models/onLine/libs/sockets.js
+++ src/models/onLine/libs/sockets.js
@@ -1,18 +1,22 @@
-import socketIOClient from "socket.io-client";
+import socketIOClient from 'socket.io-client';
 
-const ENTRY_POINT = "http://127.0.0.1";
+const ENTRY_POINT = 'http://164.90.222.66';
 
-const createSocketConnection = async ({nameSpace})=>{
-  const url = nameSpace ? ENTRY_POINT+nameSpace : ENTRY_POINT;
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>createSocketConnection  url',url)
+const createSocketConnection = async ({ nameSpace }) => {
+  const url = nameSpace ? ENTRY_POINT + nameSpace : ENTRY_POINT;
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>createSocketConnection  url', url);
   const socket = socketIOClient(url);
-  return await new Promise((resolve)=>{
-      socket.on("connection", (socketData) => {
-        socketData.socket = socket;
-        console.log(">>>>>>>>>>>>>>>>>>>>>>>>SERVER!!!!! SOCKET CONNECTION to url,NameSsocketData",url, socketData);
-        resolve(socketData)
-      });
+  return await new Promise((resolve) => {
+    socket.on('connection', (socketData) => {
+      socketData.socket = socket;
+      console.log(
+        '>>>>>>>>>>>>>>>>>>>>>>>>SERVER!!!!! SOCKET CONNECTION to url,NameSsocketData',
+        url,
+        socketData
+      );
+      resolve(socketData);
+    });
   });
-}
+};
 
 export { ENTRY_POINT, createSocketConnection };
