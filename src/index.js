3dcafb3d87bad1ad5547e6f8b4a2abdff867290a
Aris <arisapostoloy@gmail.com>
2020-08-02 12:59:23 +0300

    made optional the reduxDevTools middleare

--- src/index.js
+++ src/index.js
@@ -3,26 +3,27 @@ import ReactDOM from 'react-dom';
 import './static/main.scss';
 import { HomePageContainer } from './TicToe/';
 import * as serviceWorker from './serviceWorker';
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
-import { games } from "./models/tic-tac-toe/";
-import { online, rootSaga } from "./models/onLine/";
-import createSagaMiddleware from "redux-saga";
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { games } from './models/tic-tac-toe/';
+import { online, rootSaga } from './models/onLine/';
+import createSagaMiddleware from 'redux-saga';
 const sagaMiddleware = createSagaMiddleware();
 const fullReducer = combineReducers({ online, games });
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : (f) => f;
 const gameStore = createStore(
-    fullReducer,
-    compose(
-     applyMiddleware(sagaMiddleware),
-     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-  );
+  fullReducer,
+  compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+);
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={gameStore}>
     <HomePageContainer />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
